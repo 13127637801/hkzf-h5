6619bbf07c681e94cfc2895f4c6f9175d94bf5fe
jn <13127637801@163.com>
2021-09-16 21:24:03 +0800

    improve

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -4,11 +4,12 @@ import { NavBar } from "antd-mobile";
 
 import { withRouter } from "react-router";
 
+import PropTypes from "prop-types";
+
 // 导入样式
 import "./index.scss";
 
 function NavHeader(props) {
-  console.log(props);
   const defaultHandler = () => props.history.go("-1");
   return (
     <NavBar
@@ -21,5 +22,11 @@ function NavHeader(props) {
     </NavBar>
   );
 }
+// 添加props校验
+NavHeader.propTypes = {
+    children: PropTypes.string.isRequired,
+    onLeftClick: PropTypes.func
+  }
+  
 // withRouter(NavHeader) 函数的返回值也是一个组件
 export default withRouter(NavHeader);
--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { NavBar, Toast } from "antd-mobile";
+import {  Toast } from "antd-mobile";
 import { getCurrentCity } from "../../utils";
 
 import { List, AutoSizer } from "react-virtualized";
+
+// 导入封装好的 NavHeader 组件
+import NavHeader from "../../components/NavHeader";
 
 // 导入axios
 import axios from "axios";
@@ -155,14 +158,9 @@ export default class CityList extends React.Component {
   render() {
     return (
       <div className="citylist">
-        <NavBar
-          className="navbar"
-          mode="light"
-          icon={<i className="iconfont icon-back" />}
-          onLeftClick={() => this.props.history.go("-1")}
-        >
-          城市选择
-        </NavBar>
+        
+        <NavHeader>城市选择</NavHeader>
+        
         {/* 城市列表 */}
         <AutoSizer>
           {({ width, height }) => (
