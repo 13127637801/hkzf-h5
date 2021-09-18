fe49ea3ffff14428f77b30222ca6dd820235b4a1
jn <13127637801@163.com>
2021-09-18 21:14:08 +0800

    cmmit

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import "./index.scss";
-import styles from "./index.module.css"
+import styles from "./index.module.css";
 // 导入封装好的 NavHeader 组件
 import NavHeader from "../../components/NavHeader";
 
@@ -11,9 +11,33 @@ export default class Map extends React.Component {
     const map = new BMapGL.Map("container");
     // 设置中心点坐标
     const point = new BMapGL.Point(116.404, 39.915);
-    // 初始化地图
-    map.centerAndZoom(point, 15);
+   
+    
+
+    const myGeo = new BMapGL.Geocoder();
+    // 获取地址
+    const localCity = JSON.parse(localStorage.getItem("hkzf_city"));
+    const { label,value } = localCity
+    // 将地址解析结果显示在地图上，并调整地图视野
+    myGeo.getPoint(
+      label,
+      (point) => {
+        if (point) {
+           // 初始化地图
+          map.centerAndZoom(point, 15);
+          map.addOverlay(
+            new BMapGL.Marker(point, { title: "北京市海淀区上地10街" })
+          );
+        } else {
+          alert("您选择的地址没有解析到结果！");
+        }
+      },
+      label
+    );
+
+   
   }
+
   render() {
     return (
       <div className={styles.map}>
