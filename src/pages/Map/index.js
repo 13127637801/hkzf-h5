243630bcd2b3a8b1f5f6f375718d4b7adce8eea8
jn <13127637801@163.com>
2021-09-19 21:57:31 +0800

    地图数据

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import styles from "./index.module.css";
 // 导入封装好的 NavHeader 组件
 import NavHeader from "../../components/NavHeader";
+// 导入axios
+import axios from "axios";
 
 // 覆盖物样式
 const labelStyle = {
@@ -34,7 +36,7 @@ export default class Map extends React.Component {
     // 将地址解析结果显示在地图上，并调整地图视野
     myGeo.getPoint(
       label,
-      (point) => {
+      async (point) => {
         if (point) {
           // 初始化地图
           map.centerAndZoom(point, 11);
@@ -44,23 +46,51 @@ export default class Map extends React.Component {
           map.addControl(new BMapGL.ZoomControl());
           map.addControl(new BMapGL.ScaleControl());
 
-          var opts = {
-            position: point, // 指定文本标注所在的地理位置
-            offset: new BMapGL.Size(-35, -35), // 设置文本偏移量
-          };
-          const label = new BMapGL.Label("", opts);
+          const res = await axios.get("http://localhost:8080/area/map", {
+            params: {
+              id: value,
+            },
+          });
 
-          // 设置房源覆盖物内容
-          label.setContent(`
-            <div class="${styles.bubble}">
-              <p class="${styles.name}">浦东</p>
-              <p>99套</p>
-            </div>
-          `);
+          res.data.body.forEach((item) => {
+            const {
+              coord: { latitude, longitude },
+              label: areaName,
+              count,
+              value,
+            } = item;
 
-          // 设置样式
-          label.setStyle(labelStyle);
-          map.addOverlay(label);
+            const areaPoint = new BMapGL.Point(longitude, latitude);
+
+            var opts = {
+              position: areaPoint, // 指定文本标注所在的地理位置
+              offset: new BMapGL.Size(-35, -35), // 设置文本偏移量
+            };
+            const label = new BMapGL.Label("", opts);
+            // 给 label 对象添加一个唯一标识
+            label.id = value;
+
+            // 设置房源覆盖物内容
+            label.setContent(`
+              <div class="${styles.bubble}">
+                <p class="${styles.name}">${areaName}</p>
+                <p>${count}套</p>
+              </div>
+            `);
+
+            // 设置样式
+            label.setStyle(labelStyle);
+            label.addEventListener("click", () => {
+             
+              map.centerAndZoom(areaPoint, 13);
+
+              setTimeout(() => {
+                // 清除当前覆盖物信息
+                map.clearOverlays();
+              }, 0);
+            });
+            map.addOverlay(label);
+          });
         } else {
           alert("您选择的地址没有解析到结果！");
         }
