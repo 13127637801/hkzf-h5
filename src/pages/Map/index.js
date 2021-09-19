25877f2c5ec10fa7e6f2f04f8c725dde59591fce
jn <13127637801@163.com>
2021-09-19 19:34:09 +0800

    房源覆盖物

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -4,6 +4,17 @@ import styles from "./index.module.css";
 // 导入封装好的 NavHeader 组件
 import NavHeader from "../../components/NavHeader";
 
+// 覆盖物样式
+const labelStyle = {
+  cursor: "pointer",
+  border: "0px solid rgb(255, 0, 0)",
+  padding: "0px",
+  whiteSpace: "nowrap",
+  fontSize: "12px",
+  color: "rgb(255, 255, 255)",
+  textAlign: "center",
+};
+
 export default class Map extends React.Component {
   componentDidMount() {
     this.initMap();
@@ -32,6 +43,24 @@ export default class Map extends React.Component {
           // );
           map.addControl(new BMapGL.ZoomControl());
           map.addControl(new BMapGL.ScaleControl());
+
+          var opts = {
+            position: point, // 指定文本标注所在的地理位置
+            offset: new BMapGL.Size(-35, -35), // 设置文本偏移量
+          };
+          const label = new BMapGL.Label("", opts);
+
+          // 设置房源覆盖物内容
+          label.setContent(`
+            <div class="${styles.bubble}">
+              <p class="${styles.name}">浦东</p>
+              <p>99套</p>
+            </div>
+          `);
+
+          // 设置样式
+          label.setStyle(labelStyle);
+          map.addOverlay(label);
         } else {
           alert("您选择的地址没有解析到结果！");
         }
