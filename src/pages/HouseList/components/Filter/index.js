ac08091c327bd21edaacf3476690a079d25fc4e4
jn <13127637801@163.com>
2021-09-24 22:57:37 +0800

    默认选择项

--- src/pages/HouseList/components/Filter/index.js
+++ src/pages/HouseList/components/Filter/index.js
@@ -5,7 +5,8 @@ import FilterPicker from "../FilterPicker";
 import FilterMore from "../FilterMore";
 
 import styles from "./index.module.css";
-
+// 导入axios
+import { API } from "../../../../utils/api";
 // 标题高亮状态
 // true 表示高亮； false 表示不高亮
 const titleSelectedStatus = {
@@ -15,29 +16,110 @@ const titleSelectedStatus = {
   more: false,
 };
 
+// FilterPicker 和 FilterMore 组件的选中值
+const selectedValues = {
+  area: ["area", "null"],
+  mode: ["null"],
+  price: ["null"],
+  more: [],
+};
+
 export default class Filter extends Component {
   state = {
     titleSelectedStatus,
+    openType: "",
+    // 所有筛选条件数据
+    filtersData: {},
+    selectedValues,
   };
+  componentDidMount() {
+    this.getFiltersData();
+  }
   onTitleClick = (type) => {
     this.setState((prevState) => {
-      console.log(prevState)
       return {
         titleSelectedStatus: {
           ...prevState.titleSelectedStatus,
-          [type]: true
+          [type]: true,
         },
+        openType: type,
       };
     });
   };
-  render() {
-    const { titleSelectedStatus } = this.state;
+  onCancel = () => {
+    this.setState({
+      openType: "",
+    });
+  };
+  onSave = (type, value) => {
+    console.log(type, value);
+    this.setState({
+      openType: "",
+      selectedValues: {
+        ...selectedValues,
+        [type]: value,
+      },
+    });
+  };
+  // 封装获取所有筛选条件的方法
+  async getFiltersData() {
+    // 获取当前定位城市id
+    const { value } = JSON.parse(localStorage.getItem("hkzf_city"));
+    const res = await API.get(`/houses/condition?id=${value}`);
+    this.setState({
+      filtersData: res.data.body,
+    });
+    console.log(this.state.filtersData);
+  }
+  renderFilterPicker() {
+    const {
+      openType,
+      filtersData: { area, subway, rentType, price },
+      selectedValues,
+    } = this.state;
 
+    if (openType !== "area" && openType !== "mode" && openType !== "price") {
+      return null;
+    }
+    let data = [];
+    let cols = 3;
+    let defaultValue = selectedValues[openType];
+    switch (openType) {
+      case "area":
+        data = [area, subway];
+        cols = 3;
+        break;
+      case "mode":
+        data = rentType;
+        cols = 1;
+        break;
+      case "price":
+        data = price;
+        cols = 1;
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <FilterPicker
+        onCancel={this.onCancel}
+        onSave={this.onSave}
+        data={data}
+        cols={cols}
+        type={openType}
+        defaultValue={defaultValue}
+      />
+    );
+  }
+  render() {
+    const { titleSelectedStatus, openType } = this.state;
     return (
       <div className={styles.root}>
         {/* 前三个菜单的遮罩层 */}
-        {/* <div className={styles.mask} /> */}
-
+        {openType === "area" || openType === "mode" || openType === "price" ? (
+          <div className={styles.mask} onClick={this.onCancel} />
+        ) : null}
         <div className={styles.content}>
           {/* 标题栏 */}
           <FilterTitle
@@ -46,7 +128,7 @@ export default class Filter extends Component {
           />
 
           {/* 前三个菜单对应的内容： */}
-          {/* <FilterPicker /> */}
+          {this.renderFilterPicker()}
 
           {/* 最后一个菜单对应的内容： */}
           {/* <FilterMore /> */}
