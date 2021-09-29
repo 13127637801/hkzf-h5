e945e134bc14b41e694aaca90cc9dd759a6588e0
jn <13127637801@163.com>
2021-09-29 22:06:50 +0800

    删除严格模式

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 // 导入样式
-import 'antd-mobile/dist/antd-mobile.css';
+import "antd-mobile/dist/antd-mobile.css";
 
 // 导入字体图标库的样式文件
-import './assets/fonts/iconfont.css';
+import "./assets/fonts/iconfont.css";
 // 导入react-virtualized样式
-import 'react-virtualized/styles.css';
+import "react-virtualized/styles.css";
 
-import './index.css';
+import "./index.css";
 
-import App from './App';
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
