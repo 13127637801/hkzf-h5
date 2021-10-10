936b813711a0a8aadd27ab010e3cc5ac55efe6fc
jn <13127637801@163.com>
2021-10-10 21:05:13 +0800

    commit

--- src/pages/Rent/Search/index.js
+++ src/pages/Rent/Search/index.js
@@ -9,23 +9,51 @@ import styles from "./index.module.css";
 export default class Search extends Component {
   // 当前城市id
   cityId = getCity().value;
+  // 定时器id
+  timerId = null;
 
   state = {
+    // 搜索框的值
     searchTxt: "",
     tipsList: [],
+  };
+
+  /* 
+    传递小区数据：
+
+    1 给搜索列表项添加单击事件。
+    2 在事件处理程序中，调用 history.replace() 方法跳转到发布房源页面。
+    3 将被点击的小区信息作为数据一起传递过去。
+
+    4 在发布房源页面，判断 history.loaction.state 是否为空。
+    5 如果为空，不做任何处理。
+    6 如果不为空，则将小区信息存储到发布房源页面的状态中。
+  */
+  onTipsClick = (item) => {
+    this.props.history.replace("/rent/add", {
+      name: item.communityName,
+      id: item.community,
+    });
   };
 
   // 渲染搜索结果列表
   renderTips = () => {
     const { tipsList } = this.state;
-    console.log(tipsList)
+
     return tipsList.map((item) => (
-      <li key={item.value} className={styles.tip} onClick={() => this.onTipsClick(item)}>
-        {item.label}
+      <li
+        key={item.community}
+        className={styles.tip}
+        onClick={() => this.onTipsClick(item)}
+      >
+        {item.communityName}
       </li>
     ));
   };
 
+  /* 
+    关键词搜索小区信息
+  */
   handleSearchTxt = (value) => {
     this.setState({ searchTxt: value });
 
@@ -56,13 +84,6 @@ export default class Search extends Component {
     }, 500);
   };
 
-  onTipsClick = item => {
-    this.props.history.replace('/rent/add', {
-      name: item.communityName,
-      id: item.community
-    })
-  }
-
   render() {
     const { history } = this.props;
     const { searchTxt } = this.state;
@@ -73,9 +94,9 @@ export default class Search extends Component {
         <SearchBar
           placeholder="请输入小区或地址"
           value={searchTxt}
-          showCancelButton={true}
           onChange={this.handleSearchTxt}
-          onCancel={() => history.go(-1)}
+          showCancelButton={true}
+          onCancel={() => history.replace("/rent/add")}
         />
 
         {/* 搜索提示列表 */}
